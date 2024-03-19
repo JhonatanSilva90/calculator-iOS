6c46b4d3946f78b974b1e852167cec2597167076
Jhonatan <jhonatan.dev90@gmail.com>
2024-03-19 19:00:25 -0300

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Jhonatan on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
